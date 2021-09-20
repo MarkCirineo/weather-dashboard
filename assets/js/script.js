7f3f4029ccc57c52b9d55a86b6443fcf2d93df80
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-19 22:38:58 -0400

    added function to create buttons with past searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
 var cityInput = $("input");
 var searchBtn = $("button");
+var pastSearches = $(".past-searches");
 
 searchBtn.on("click", saveInput);
 cityInput.on("keydown", saveInputOnEnter);
 
 function saveInput() {
     localStorage.setItem(cityInput.val().toUpperCase(), cityInput.val())
+        makeButtons();
         cityInput.val("") 
 }
 
 function saveInputOnEnter(e) {
     var key = e.key;
-    console.log(key)
     if (key === "Enter") {
         localStorage.setItem(cityInput.val().toUpperCase(), cityInput.val())
-        cityInput.val("") 
+        makeButtons();
+        cityInput.val("")
     }
+}
+
+function makeButtons() {
+    var pastCity = $("<button>")
+    pastCity.text(cityInput.val());
+    pastCity.attr("class", "btn btn-secondary")
+    pastSearches.append(pastCity);
 }
