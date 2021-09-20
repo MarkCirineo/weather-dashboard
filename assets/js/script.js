3c9532c313fdf7a941b59ead4a25b5337a5a24b6
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-19 22:50:59 -0400

    added function to fetch weather data when using past searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,34 @@ function saveInputOnEnter(e) {
     }
 }
 
+var pastCity;
+
 function makeButtons() {
     if (pastSearches.children().text().includes(cityInput.val())) { 
         return;
     }
-    var pastCity = $("<button>")
+    pastCity = $("<button>")
     pastCity.text(cityInput.val());
     pastCity.attr("id", cityInput.val().toLowerCase())
     pastCity.attr("class", "btn btn-secondary")
     pastSearches.append(pastCity);
+    var cityButton = $(pastCity)
+    cityButton.on("click", getWeatherFromButton)
 }
+
+// //! API KEY: 82bef0fd1f19abbff878e3a5939a1627
+function getWeatherFromButton() {
+    var city = pastCity.attr("id");
+    console.log(city);
+    
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial&appid=82bef0fd1f19abbff878e3a5939a1627"
+    fetch(url)
+        .then(function (response) {
+            console.log(response)
+            return response.json();
+        })
+            .then(function (data) {
+                console.log(data);
+            })
+} 
 
